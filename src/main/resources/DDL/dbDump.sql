27a2a03267718c5994f35b8b40891c33b79268e3
Sotiris Blad <sotiris2009@hotmail.com>
2022-05-23 21:25:19 +0300

    Update dbDump.sql

--- src/main/resources/DDL/dbDump.sql
+++ src/main/resources/DDL/dbDump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: instructordb
+-- Host: 127.0.0.1    Database: se_project
 -- ------------------------------------------------------
 -- Server version	8.0.27
 
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `ix_auth_username` (`username`,`authority`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authorities`
+--
+
+LOCK TABLES `authorities` WRITE;
+/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
+INSERT INTO `authorities` VALUES ('tsiatouxas','INSTRUCTOR'),('zarras','INSTRUCTOR');
+/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `courses`
 --
 
@@ -25,12 +49,15 @@ DROP TABLE IF EXISTS `courses`;
 CREATE TABLE `courses` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `instructor` varchar(45) DEFAULT NULL,
+  `instructor` int NOT NULL,
   `syllabus` varchar(255) DEFAULT NULL,
   `year` int NOT NULL,
   `semester` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `exam_weight` double DEFAULT '0',
+  `project_weight` double DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `instructor` (`instructor`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,8 +66,36 @@ CREATE TABLE `courses` (
 
 LOCK TABLES `courses` WRITE;
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,'SE','zarras','KAAARK',4,4),(2,'VHDL','tsiatouxas','KAAARK',4,4);
+INSERT INTO `courses` VALUES (1,'SE',1,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',2022,8,0,0),(2,'SoftDev2',1,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',2022,7,0,0),(3,'VHDL',2,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',2019,4,0,0);
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `instructors`
+--
+
+DROP TABLE IF EXISTS `instructors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `instructors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(45) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `instructors`
+--
+
+LOCK TABLES `instructors` WRITE;
+/*!40000 ALTER TABLE `instructors` DISABLE KEYS */;
+INSERT INTO `instructors` VALUES (1,'zarras','zarras','$2a$12$27Lk9T4N8ODnSRgdFyIqweGfjx0vB8iVGbBkgaGXJloFY5ojg3kK2',1),(2,'tsiatouxas','tsiatouxas','$2a$12$rg7hDGYsOqWkSjb3zGBjcuBWqzsUNIX0K.mjcbdLn.ZO3irBLXlq.',1);
+/*!40000 ALTER TABLE `instructors` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -52,14 +107,17 @@ DROP TABLE IF EXISTS `student_registrations`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `student_registrations` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `student_id` int NOT NULL,
   `name` varchar(45) NOT NULL,
   `year_of_registration` int NOT NULL,
   `semester` int NOT NULL,
   `course_id` int NOT NULL,
+  `grade` double DEFAULT '0',
+  `project_grade` double DEFAULT '0',
+  `exam_grade` double DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `course_id` (`course_id`),
-  CONSTRAINT `student_registrations_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `course_id` (`course_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +126,7 @@ CREATE TABLE `student_registrations` (
 
 LOCK TABLES `student_registrations` WRITE;
 /*!40000 ALTER TABLE `student_registrations` DISABLE KEYS */;
+INSERT INTO `student_registrations` VALUES (1,1,'sotblad',2018,8,1,0,0,0),(2,2,'stratis',2018,8,1,0,0,0),(3,2,'stratis',2018,8,3,0,0,0),(4,3,'jim',2018,8,3,0,0,0),(5,1,'sotblad',2018,8,2,0,0,0),(6,2,'stratis',2018,8,2,0,0,0),(7,3,'jim',2018,8,2,0,0,0);
 /*!40000 ALTER TABLE `student_registrations` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-18 15:48:30
+-- Dump completed on 2022-05-23 21:24:42
